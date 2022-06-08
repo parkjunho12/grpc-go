b7c5a435268affef2fca0969355df75956298c8c
parkjunho12 <ghdlwnsgh12@naver.com>
2022-06-08 14:31:15 +0900

    Ad

--- examples/helloworld/greeter_server/main.go
+++ examples/helloworld/greeter_server/main.go
@@ -42,7 +42,7 @@ type server struct {
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "hi " + in.GetName()}, nil
+	return &pb.HelloReply{Message: "hi" + in.GetName()}, nil
 }
 
 func main() {
